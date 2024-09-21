5a790ca14f3b39194bd5a7c2f1e3708007ba5fa8
Seymur <seymur.2004.78@mail.ru>
2024-09-21 15:54:11 +0400

    Create SalamController.cs
    new controller

---
+++ Controllers/SalamController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MinimalCourseCRUD.Controllers
+{
+    public class SalamController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
